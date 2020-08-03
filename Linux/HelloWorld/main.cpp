00f6135eafccb745a0c3344a5dc666e59c4e10cb
mohan <mohan.darsi@yahoo.com>
2020-08-03 22:58:59 +0200

    using lldb

--- Linux/HelloWorld/main.cpp
+++ Linux/HelloWorld/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <vector>
+#include <memory>
+#include <sstream>
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
-    for(int i=0;i<5;i++)
+    auto vecStr = std::make_unique<std::vector<std::string>>();
+    for(auto i=0;i<5;i++)
     {
-        std::cout<<"Hello world :"<<i<<std::endl;
+        std::stringstream stream;
+        stream << "Hello world :" << i;
+        vecStr->push_back(stream.str());
+    }
+    for(auto &itr:*(vecStr.get()))
+    {
+        std::cout<<itr<<std::endl;
     }
 }
