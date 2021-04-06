4fe9a2474ba2bdd4de30011d13b3d2960b351692
mohandarsi <33396981+mohandarsi@users.noreply.github.com>
2021-04-06 20:41:34 +0200

    Update SIMD_Image.cpp

--- SIMD_Image.cpp
+++ SIMD_Image.cpp
@@ -1,4 +1,3 @@
-// Type your code here, or load an example.
 #include <immintrin.h>
 #include <iostream>
 #include <stdio.h>
@@ -7,13 +6,20 @@
 #include <ctime>
 #include <chrono>
 
-#define ARRAY_LENGTH 8
-
 void fillRandomImage(std::vector<unsigned char> &image)
 {
     std::srand(unsigned(std::time(nullptr)));
-
     std::generate(image.begin(), image.end(), std::rand);
+}
+
+void print(std::vector<unsigned char> &image)
+{
+   //auto test =_mm256_load_si256((__m256i*)&image[0]);
+   for (unsigned short i = 0; i < 30; i += 1)
+   {
+        printf("%d ", image[i]);
+   }
+   std::cout<< std::endl;
 }
 
 int main(int argc, char* argv[]) {
@@ -31,19 +37,14 @@ int main(int argc, char* argv[]) {
         image3[i] = image1[i] + image2[i];
     }
     auto end = std::chrono::steady_clock::now();
+ 
+    print(image3);
 
-    for (
-        unsigned short i = 0;
-        i < 30;
-        i += 1
-    ) {
-        printf("%d ", image3[i]);
-    }
-    std::fill(image3.begin(), image3.end(), 0);
-    std::cout<< std::endl;
-    
     std::cout<<"Scalar loop Time taken:"<< (std::chrono::duration_cast<std::chrono::milliseconds>(end-begin)).count() << "ms" << std::endl;
-
+   
+    std::fill(image3.begin(), image3.end(), 0);
+    print(image3);
+    
     begin = std::chrono::steady_clock::now();
     for (auto i = 0; i+16 < SIZE; i += 16)
     {
@@ -54,23 +55,16 @@ int main(int argc, char* argv[]) {
     }
     end = std::chrono::steady_clock::now();
 
-    for (
-        unsigned short i = 0;
-        i < 30;
-        i += 1
-    ) {
-        printf("%d ", image3[i]);
-    }
-   std::cout<< std::endl;
+    print(image3);
     std::cout<<"vector loop Time taken:"<< (std::chrono::duration_cast<std::chrono::milliseconds>(end-begin)).count() << "ms" << std::endl;
 
-
+    #define ARRAY_LENGTH 8
     auto first = _mm_set_epi16(10, 20, 30, 40, 50, 60, 70, 80);
     auto second = _mm_set_epi16(5, 5, 5, 5, 5, 5, 5, 5);
     auto result = _mm_add_epi16(first, second);
 
     short* values = (short*) &result;
-
+   
     for (
         unsigned short i = 0;
         i < ARRAY_LENGTH;
